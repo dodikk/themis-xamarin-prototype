bf4ea47e399e4c7a7cc53014db4502c64a42554e
dodikk <dodikk88.reg@gmail.com>
2019-11-27 21:30:22 +0200

    [bind] [droid] added droid sample code from themis docs
    https://docs.cossacklabs.com/pages/java-and-android-howto/#secure-cell

--- app/ThemisDemoXamarin.Android/MainActivity.cs
+++ app/ThemisDemoXamarin.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Com.Cossacklabs.Themis;
+using System.Linq;
 
 namespace ThemisDemoXamarin.Droid
 {
@@ -14,6 +16,8 @@ namespace ThemisDemoXamarin.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            ExecuteThemisNativeSample();
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -23,11 +27,65 @@ namespace ThemisDemoXamarin.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+
+        public override void OnRequestPermissionsResult(
+            int requestCode,
+            string[] permissions,
+            [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void ExecuteThemisNativeSample()
+        {
+            string masterKeyString = "UkVDMgAAAC13PCVZAKOczZXUpvkhsC+xvwWnv3CLmlG0Wzy8ZBMnT+2yx/dg";
+            var secureCell = new SecureCell(masterKeyString);
+
+            string plainTextMessage = "Droid binding plain text message";
+            Console.WriteLine($"Initial Text: {plainTextMessage}");
+
+            
+
+            Console.WriteLine("Encrypting...");
+
+            byte[] plainTextMessageData =
+                plainTextMessage
+                    .ToCharArray()
+                    .Select((ch) => (byte)ch)
+                    .ToArray();
+
+            SecureCellData cypherTextHolder =
+                secureCell.Protect(
+                    context: "no context",
+                    data: plainTextMessageData);
+
+            Console.WriteLine("Done.");
+
+            // convert cyphertext for printing
+            // -
+            byte[] cypherText = cypherTextHolder.GetProtectedData();
+            char[] cypherTextChars = cypherText.Select(b => (char)b).ToArray();
+
+            string txtCypherText =
+                new string(
+                    value: cypherTextChars,
+                    startIndex: 0,
+                    length: cypherTextChars.Length);
+            Console.WriteLine($"Cypher Text: {txtCypherText}");
+
+
+            byte[] decryptedData = secureCell.Unprotect(
+                context: "no context",
+                protectedData: cypherTextHolder);
+            char[] decryptedDataChars = decryptedData.Select(b => (char)b).ToArray();
+            string decryptedText =
+                new string(
+                    value: decryptedDataChars,
+                    startIndex: 0,
+                    length: decryptedDataChars.Length);
+            Console.WriteLine($"Decrypted Text: {decryptedText}");
         }
     }
 }
