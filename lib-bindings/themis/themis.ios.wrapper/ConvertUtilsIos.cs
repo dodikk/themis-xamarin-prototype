fc6fef4497b3461c89f2a0b381afd70d7116ebc2
dodikk <dodikk88.reg@gmail.com>
2019-12-20 16:38:35 +0200

    [.net] extracted string==>stream==>bytes conversion utils

---
+++ lib-bindings/themis/themis.ios.wrapper/ConvertUtilsIos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Foundation;
+
+
+namespace Themis.iOS.Utils
+{
+    public static class ConvertUtilsIos
+    {
+        public static NSData StreamToNSData(Stream stream)
+        {
+            if (stream == null)
+            {
+                return null;
+            }
+
+            byte[] streamBytes = ConvertUtilsPortable.StreamToByteArray(stream);
+            NSData result = ByteArrayToNSData(streamBytes);
+
+            return result;
+        }
+
+        public static NSData ByteArrayToNSData(byte[] memoryBuffer)
+        {
+            if (memoryBuffer == null)
+            {
+                return null;
+            }
+
+            string base64ForBuffer = Convert.ToBase64String(memoryBuffer);
+
+            NSData result =
+                new NSData(
+                    base64String: base64ForBuffer,
+                    options: NSDataBase64DecodingOptions.None);
+
+            return result;
+        }
+
+        public static byte[] NSDataToByteArray(NSData iosData)
+        {
+            if (iosData == null)
+            {
+                return null;
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            using (Stream nsDataStream = iosData.AsStream())
+            {
+                nsDataStream.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ lib-bindings/themis/themis.pcl/ConvertUtilsPortable.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.IO;
+
+
+namespace Themis
+{
+    public static class ConvertUtilsPortable
+    {
+        public static byte[] StreamToByteArray(Stream stream)
+        {
+            if (stream == null)
+            {
+                return null;
+            }
+
+            using (var memStream = new MemoryStream())
+            {
+                stream.CopyTo(memStream);
+                byte[] result = memStream.GetBuffer();
+
+                return result;
+            }
+        }
+
+        public static MemoryStream ByteArrayToMemoryStream(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            var result =
+                new MemoryStream(
+                    buffer: data,
+                    index: 0,
+                    count: data.Length,
+                    writable: false,
+                    publiclyVisible: true);
+
+            return result;
+        }
+
+        public static MemoryStream StringToMemoryStream(string str)
+        {
+            byte[] data = StringToByteArray(str);
+            MemoryStream result = ByteArrayToMemoryStream(data);
+
+            return result;
+        }
+
+        public static byte[] StringToByteArray(string str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            byte[] result =
+                str.ToCharArray()
+                   .Select(c => (byte)c)
+                   .ToArray();
+
+            return result;
+        }
+
+        public static string ByteArrayToString(byte[] stringAsTextData)
+        {
+            if (stringAsTextData == null)
+            {
+                return null;
+            }
+
+            char[] stringAsTextChars =
+                stringAsTextData.Select(b => (char)b)
+                                .ToArray();
+
+            string result =
+                new string(
+                    value: stringAsTextChars,
+                    startIndex: 0,
+                    length: stringAsTextChars.Length);
+
+            return result;
+        }
+    }
+}
