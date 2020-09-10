88ae0f8faa82d1fa748c290fc519d4b0fe09ab7e
dodikk <dodikk88.reg@gmail.com>
2020-09-10 22:56:26 +0300

    [v0.13.2] added missing curl flag for droid

--- scripts/download-binaries-droid.sh
+++ scripts/download-binaries-droid.sh
@@ -9,10 +9,10 @@ cd ../lib-native/droid-aar
 ## https://bintray.com/cossacklabs/maven/themis/0.12.0#
 
 
-curl --output themis-0.13.1.aar https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar
-curl --output themis-0.13.1.aar.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar.asc
-curl --output themis-0.13.1.pom https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom
-curl --output themis-0.13.1.pom.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom.asc
+curl -L --output themis-0.13.1.aar https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar
+curl -L --output themis-0.13.1.aar.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar.asc
+curl -L --output themis-0.13.1.pom https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom
+curl -L --output themis-0.13.1.pom.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom.asc
 
 
 
