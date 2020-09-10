31ad79739d2cacfa3d32937eb6afa0d1829482ac
dodikk <dodikk88.reg@gmail.com>
2020-09-10 21:24:49 +0300

    [v0.13.2] updated download script for droid

--- scripts/download-binaries-droid.sh
+++ scripts/download-binaries-droid.sh
@@ -8,9 +8,9 @@ cd ../lib-native/droid-aar
 
 ## https://bintray.com/cossacklabs/maven/themis/0.12.0#
 
-wget --output-document=themis-0.12.0.aar https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.12.0%2Fthemis-0.12.0.aar
-wget --output-document=themis-0.12.0.aar.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.12.0%2Fthemis-0.12.0.aar.asc
-wget --output-document=themis-0.12.0.pom https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.12.0%2Fthemis-0.12.0.pom
-wget --output-document=themis-0.12.0.pom.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.12.0%2Fthemis-0.12.0.pom.asc
+wget --output-document=themis-0.13.1.aar https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar
+wget --output-document=themis-0.13.1.aar.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.aar.asc
+wget --output-document=themis-0.13.1.pom https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom
+wget --output-document=themis-0.13.1.pom.asc https://bintray.com/cossacklabs/maven/download_file?file_path=com%2Fcossacklabs%2Fcom%2Fthemis%2F0.13.1%2Fthemis-0.13.1.pom.asc
 
 cd "$SCRIPT_START_DIR"
