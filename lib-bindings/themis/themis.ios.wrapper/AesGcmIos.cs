006b7281db0661e4854e0d5fb20497c8cd35683c
dodikk <dodikk88.reg@gmail.com>
2020-09-11 14:11:10 +0300

    added AES-GCM bindings
    .netstandard2.0 does not support that cipher in a portable way
    so xamarin needs explicit bindings for that

---
+++ lib-bindings/themis/themis.ios.wrapper/AesGcmIos.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace Themis.iOS
+{
+    public class AesGcmIos: IAesGcmBlockCipher
+    {
+        private const int BitsInSingleByte = 8;
+
+        public byte[] Decrypt(
+            byte[] key,
+            byte[] cypherTextDataWithTagSuffix,
+            AesGcmBlockCipherConfig options)
+        {
+            if (options.TagLengthInBits < BitsInSingleByte)
+            {
+                throw new ArgumentException(nameof(options.TagLengthInBits));
+            }
+            int tagSizeInBytes = options.TagLengthInBits / BitsInSingleByte;
+
+
+            int ciphertextLength = cypherTextDataWithTagSuffix.Length - tagSizeInBytes;
+            if (ciphertextLength <= 0)
+            {
+                throw new ArgumentException(nameof(options.TagLengthInBits));
+            }
+
+
+            byte[] cypherTextData = cypherTextDataWithTagSuffix.AsSpan(
+                start: 0,
+                length: ciphertextLength).ToArray();
+
+            byte[] tagData = cypherTextDataWithTagSuffix.AsSpan(
+                start: ciphertextLength,
+                length: tagSizeInBytes).ToArray();
+
+            // https://crypto.stackexchange.com/questions/26783/ciphertext-and-tag-size-and-iv-transmission-with-aes-in-gcm-mode
+            // -
+            // Output size = input size That's correct, GCM uses CTR internally.
+            // It encrypts a counter value for each block,
+            // but it only uses as many bits as required from the last block.
+            // CTR turns the block cipher into a stream cipher.
+            // Note that this doesn't include the optional additional authenticated data
+            // (AAD), the optional IV nor the required authentication tag.
+            // -
+            byte[] outPlainText =
+                (byte[])Array.CreateInstance(
+                    elementType: typeof(byte),
+                    length: cypherTextData.Length);
+
+            using (var cipher = new AesGcm(key: key))
+            {
+                cipher.Decrypt(
+                    nonce: options.InitializationVector,
+                    ciphertext: cypherTextData,
+                    tag: tagData,
+                    plaintext: outPlainText,
+                    associatedData: null);
+
+                return outPlainText;
+            }
+        }
+
+        public byte[] Encrypt(
+            byte[] key,
+            byte[] plainTextData,
+            AesGcmBlockCipherConfig options)
+        {
+            if (options.TagLengthInBits < BitsInSingleByte)
+            {
+                throw new ArgumentException(nameof(options.TagLengthInBits));
+            }
+            int tagSizeInBytes = options.TagLengthInBits / BitsInSingleByte;
+
+
+            using (var cipher = new AesGcm(key: key))
+            {
+                byte[] outTagBuffer =
+                    (byte[])Array.CreateInstance(
+                        elementType: typeof(byte),
+                        length: tagSizeInBytes);
+
+                // https://crypto.stackexchange.com/questions/26783/ciphertext-and-tag-size-and-iv-transmission-with-aes-in-gcm-mode
+                // -
+                // Output size = input size That's correct, GCM uses CTR internally.
+                // It encrypts a counter value for each block,
+                // but it only uses as many bits as required from the last block.
+                // CTR turns the block cipher into a stream cipher.
+                // Note that this doesn't include the optional additional authenticated data
+                // (AAD), the optional IV nor the required authentication tag.
+                // -
+                byte[] outCiphertext =
+                    (byte[])Array.CreateInstance(
+                        elementType: typeof(byte),
+                        length: plainTextData.Length);
+
+                cipher.Encrypt(
+                    nonce: options.InitializationVector,
+                    plaintext: plainTextData,
+                    ciphertext: outCiphertext,
+                    tag: outTagBuffer,
+                    associatedData: null);
+
+                // Note: [alex-d] Seems like tag is appended to encrypted text by JS
+                // so we'll do that as well
+                // https://github.com/bitwiseshiftleft/sjcl/blob/master/core/gcm.js#L34
+                // https://github.com/bitwiseshiftleft/sjcl/blob/master/core/gcm.js#L51
+                // https://github.com/rynomad/subtle
+                // https://github.com/bitwiseshiftleft/sjcl
+                // -
+                // P.S. java does that too on android
+                // -
+
+                byte[] result = outCiphertext.Concat(outTagBuffer).ToArray();
+                return result;
+            }
+        }
+    }
+}
---
+++ lib-bindings/themis/themis.pcl/IAesGcmBlockCipher.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Themis
+{
+    public interface IAesGcmBlockCipher
+    {
+        byte[] Encrypt(byte[] key, byte[] plainTextData, AesGcmBlockCipherConfig options);
+        byte[] Decrypt(byte[] key, byte[] cypherTextDataWithTagSuffix, AesGcmBlockCipherConfig options);
+    }
+
+    public struct AesGcmBlockCipherConfig
+    {
+        public byte[] InitializationVector { get; set; }
+        public int TagLengthInBits { get; set; }
+    }
+}
