6a09acbe384587e846d0472d55e83221ee1f3573
dodikk <dodikk88.reg@gmail.com>
2021-03-17 19:39:03 +0200

    [v0.13.6] added native ios framework and CellSeal test code

--- app/ThemisDemoNative/ThemisDemoNative/AppDelegate.swift
+++ app/ThemisDemoNative/ThemisDemoNative/AppDelegate.swift
@@ -6,17 +6,45 @@
 //
 
 import UIKit
+import themis
+
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    func application(         _ application: UIApplication,
+didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        executeThemisNativeSample()
+        
         return true
     }
 
+    func executeThemisNativeSample()
+    {
+        let masterKey = "UkVDMgAAAC13PCVZAKOczZXUpvkhsC+xvwWnv3CLmlG0Wzy8ZBMnT+2yx/dg";
+        let masterKeyData = Data(
+            base64Encoded:masterKey,
+            options: .ignoreUnknownCharacters);
+        
+        let cellSeal = TSCellSeal(masterKey: masterKeyData);
+        let initialMessagePlainText = "iOS native project plain text message";
+        print("initial text: \(initialMessagePlainText)");
+        
+        let cipherText = cellSeal.encrypt(message: initialMessagePlainText);
+        print("encrypted text: \(cipherText)");
+        
+        let resultPlainTextData = cellSeal.decrypt(cipherText);
+        
+        let resultPlainText = String(
+            data: resultPlainTextData,
+            encoding: .utf8)!
+        print("decryptedText : \(resultPlainText)")
+        
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
